52c42cafc46bd49c6fb509bce11ee9e9e85eec15
Sagar Vijayagiri <sagarvijayagiri@gmail.com>
2025-05-23 14:14:34 +0530

    gggg

--- routes/video.js
+++ routes/video.js
@@ -10,15 +10,7 @@ cloudinary.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 });
-router.get('/:id', async (req, res) => {
-  try {
-    const video = await video.findById(req.params.id)
-    if (!video) return res.status(404).json({ error: 'Video not found' })
-    res.json({ video })
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
+
 
 router.get('/ownvideo',checkauth,async(req,res)=>{
   try{
@@ -234,6 +226,15 @@ router.put("/views/:videoid",async(req,res)=>{
     res.stsatus(500).json({
       error:err.message
     })
+  }
+})
+router.get('/:id', async (req, res) => {
+  try {
+    const video = await video.findById(req.params.id)
+    if (!video) return res.status(404).json({ error: 'Video not found' })
+    res.json({ video })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
   }
 })
 
